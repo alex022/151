54fc1e111d6f187105aac5377e78d33ac68572e6
alex022 <alex.ch776@gmail.com>
2014-05-07 14:43:51 -0700

    Added socks

--- src/RAnode.java
+++ src/RAnode.java
@@ -1,4 +1,12 @@
+import java.io.DataInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 
 public class RAnode {
+	
+	Socket socket;
+	DataInputStream dis; 
+	InetAddress address; 
 
 }
