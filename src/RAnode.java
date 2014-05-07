a18a9730adda81a3f4d12d3d6a6941321de2395e
alex022 <alex.ch776@gmail.com>
2014-05-07 14:51:22 -0700

    More socks

--- src/RAnode.java
+++ src/RAnode.java
@@ -1,24 +1,35 @@
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 
 public class RAnode {
 	
-	static Socket socket;
+	static Socket clientSocket;
+	static ServerSocket serverSocket;
 	DataInputStream dis; 
 	InetAddress address; 
 	static int portNumber;
 	
 	public static void main(String[] args)
 	{
+		
+		
 		try {
-	           socket = new Socket("Machine name", portNumber);
+	           clientSocket = new Socket("Machine name", portNumber);
 	    }
 	    catch (IOException e) {
 	        System.out.println(e);
 	    }
+		
+		try {
+		       serverSocket = new ServerSocket(portNumber);
+		        }
+		        catch (IOException e) {
+		           System.out.println(e);
+		        }
 		
 		
 	}
