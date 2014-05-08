77e9fa6da9ed712965d0d4fd2694a8e11491107e
leinad0213 <danielkwak@umail.ucsb.edu>
2014-05-07 19:39:49 -0700

    ALEX rapes souls

--- src/RAnode.java
+++ src/RAnode.java
@@ -1,53 +1,56 @@
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
 
 
 public class RAnode {
+	//writers
+	PrintWriter w1;
+	PrintWriter w2;
+	PrintWriter w3;
 	
-	static Socket clientSocket;
-	static ServerSocket serverSocket; 
-	DataInputStream dis; 
-	InetAddress address; 
-	static int portNumber;
+	//readers
+	BufferedReader in1;
+	BufferedReader in2;
+	BufferedReader in3;
 	
-	public static void main(String[] args)
-	{
-		//Initialize server socket
-		try {
-		       serverSocket = new ServerSocket(portNumber);
-		        }
-		        catch (IOException e) {
-		           System.out.println(e);
-		        }
+	public RAnode() {	
 		
-		//Accept connections from clients
-		try {
-		      clientSocket = serverSocket.accept();
-		       }
-		   catch (IOException e) {
-		      System.out.println(e);
-		   }
+		//Initialize peer sockets
+		try{
+			ServerSocket firstNode;
+			ServerSocket secondNode;
+			ServerSocket thirdNode;
+			Socket first;
+			Socket second;
+			Socket third;
 			
-		//Input
-		DataInputStream serverInput;
-	    try {
-	       serverInput = new DataInputStream(clientSocket.getInputStream());
-	    }
-	    catch (IOException e) {
-	       System.out.println(e);
-	    }
-	    
-	    //Output
-	    PrintStream serverOutput;
-	    try {
-	       serverOutput = new PrintStream(clientSocket.getOutputStream());
-	    }
-	    catch (IOException e) {
-	       System.out.println(e);
-	    }
+			//Creates two client sockets and one server socket for the node
+			first = new Socket("",5000);
+			second = new Socket("",5000);
+			thirdNode = new ServerSocket(5001);
+			
+			third = thirdNode.accept();
+			
+			System.out.println("Sockets have been successfully set");
+			
+			//Creation of the writers and readers
+			w1 = new PrintWriter(first.getOutputStream(), true);
+			w2 = new PrintWriter(second.getOutputStream(), true);
+			w3 = new PrintWriter(third.getOutputStream(), true);
+			in1 = new BufferedReader(new InputStreamReader(first.getInputStream()));
+			in2 = new BufferedReader(new InputStreamReader(second.getInputStream()));
+			in3 = new BufferedReader(new InputStreamReader(third.getInputStream()));
+			
+			
+		} catch(Exception e){
+			
+		}
+	}
+	public static void main(String[] args) 
+	{
+		new RAnode();	
 	}
 }
